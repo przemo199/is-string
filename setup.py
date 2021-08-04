5f444a34cebc1cc99e6bea41327f99308ea2f31d
Przemo199 <przemo199@onet.pl>
2021-08-04 03:12:27 +0100

    Bumped up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="is-string",
-    version="0.0.4",
+    version="0.0.5",
     author="Przemek Kaminski",
     author_email="przemyslaw.m.kaminski@gmail.com",
     description="A small Python library to determine if something is a string",
