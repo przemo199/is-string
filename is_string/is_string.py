892bf23ed626a30051f8c303275961d1ea8b2b80
Przemo199 <przemo199@onet.pl>
2021-08-04 02:57:54 +0100

    Doc edited

--- is_string/is_string.py
+++ is_string/is_string.py
@@ -1,14 +1,14 @@
 def is_string(arg: any) -> bool:
-    """Checks if an argument is a valid string.
+    """Checks if an argument is a string
 
         Parameters
         ----------
         arg
-            A variable of any type to be checked.
+            A variable of any type to be checked
 
         Returns
         -------
         bool
-            True/False depending on the argument being a string.
+            True if the provided argument is a string, otherwise False
         """
     return isinstance(arg, str)
